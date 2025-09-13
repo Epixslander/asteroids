8b89a23257c9dbbd9e03262f409d2385f767901c
epixslander <dabull0@gmail.com>
2025-09-13 05:10:48 -0400

    B: added ship and ship functionality

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
-    play_game = True
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
     dt = 0
 
-    while play_game == True:
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 play_game = False
                 return
 
+        updatable.update(dt)
+
         screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+
         dt = clock.tick(60) / 1000
 
-        play_game = True
+
 
 
 
